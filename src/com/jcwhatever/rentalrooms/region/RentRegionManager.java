8dca10d4f47c96d9cac46e5af34ceb6006f8e4fb
JCThePants <JCThePants@gmail.com>
2015-01-27 04:23:35 -0800

    update uses of IGlobalRegionManager to use class type methods
    add IGlobalRegionManager#hasRegion

--- src/com/jcwhatever/rentalrooms/region/RentRegionManager.java
+++ src/com/jcwhatever/rentalrooms/region/RentRegionManager.java
@@ -25,7 +25,6 @@
 package com.jcwhatever.rentalrooms.region;
 
 import com.jcwhatever.nucleus.Nucleus;
-import com.jcwhatever.nucleus.regions.IRegion;
 import com.jcwhatever.nucleus.regions.selection.IRegionSelection;
 import com.jcwhatever.nucleus.storage.IDataNode;
 import com.jcwhatever.nucleus.utils.CollectionUtils;
@@ -45,123 +44,120 @@ import javax.annotation.Nullable;
 
 public class RentRegionManager extends NamedInsensitiveDataManager<RentRegion> {
 
-	private final Set<String> _protectedWorlds = new HashSet<String>(10);
-	private final SingleCache<Location, RentRegion> _lastRegionByLocation = new SingleCache<Location, RentRegion>();
+    private final Set<String> _protectedWorlds = new HashSet<String>(10);
+    private final SingleCache<Location, RentRegion> _lastRegionByLocation = new SingleCache<Location, RentRegion>();
 
-	public RentRegionManager(IDataNode dataNode) {
-		super(dataNode.getNode("regions"), true);
+    public RentRegionManager(IDataNode dataNode) {
+        super(dataNode.getNode("regions"), true);
 
-		List<String> worldNames = dataNode.getStringList("protected-worlds", null);
-		if (worldNames == null) {
-			_protectedWorlds.clear();
-		}
-		else {
-			_protectedWorlds.addAll(worldNames);
-		}
-	}
-	
-	public boolean isProtectedWorld(World world) {
-		return _protectedWorlds.contains(world.getName());
-	}
-	
-	public boolean removeProtectedWorld(World world) {
-		PreCon.notNull(world);
+        List<String> worldNames = dataNode.getStringList("protected-worlds", null);
+        if (worldNames == null) {
+            _protectedWorlds.clear();
+        }
+        else {
+            _protectedWorlds.addAll(worldNames);
+        }
+    }
 
-		return removeProtectedWorld(world.getName());
-	}
-	
-	public boolean removeProtectedWorld(String worldName) {
-		PreCon.notNull(worldName);
+    public boolean isProtectedWorld(World world) {
+        return _protectedWorlds.contains(world.getName());
+    }
 
-		if (_protectedWorlds.remove(worldName)) {
+    public boolean removeProtectedWorld(World world) {
+        PreCon.notNull(world);
 
-			assert _dataNode != null;
-			_dataNode.set("protected-worlds", new ArrayList<String>(_protectedWorlds));
-			_dataNode.save();
+        return removeProtectedWorld(world.getName());
+    }
 
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean addProtectedWorld(World world) {
-		if (_protectedWorlds.add(world.getName())) {
-			assert _dataNode != null;
-			_dataNode.set("protected-worlds", new ArrayList<String>(_protectedWorlds));
-			_dataNode.save();
-			return true;
-		}
-		return false;
-	}
-	
-	
-	public List<String> getProtectedWorlds() {
-		return CollectionUtils.unmodifiableList(_protectedWorlds);
-	}
+    public boolean removeProtectedWorld(String worldName) {
+        PreCon.notNull(worldName);
+
+        if (_protectedWorlds.remove(worldName)) {
+
+            assert _dataNode != null;
+            _dataNode.set("protected-worlds", new ArrayList<String>(_protectedWorlds));
+            _dataNode.save();
+
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addProtectedWorld(World world) {
+        if (_protectedWorlds.add(world.getName())) {
+            assert _dataNode != null;
+            _dataNode.set("protected-worlds", new ArrayList<String>(_protectedWorlds));
+            _dataNode.save();
+            return true;
+        }
+        return false;
+    }
+
+    public List<String> getProtectedWorlds() {
+        return CollectionUtils.unmodifiableList(_protectedWorlds);
+    }
 
     @Nullable
-	public RentRegion get(Location location) {
-		
-		if (_lastRegionByLocation.keyEquals(location))
-			return _lastRegionByLocation.getValue();
-		
-		List<IRegion> regions = Nucleus.getRegionManager().getRegions(location);
-		
-		for (IRegion readOnlyRegion : regions) {
+    public RentRegion get(Location location) {
 
-			RentRegion region = readOnlyRegion.getMeta(RentRegion.REGION_META_KEY);
-			if (region != null) {
-				_lastRegionByLocation.set(location, region);
-				return region;
-			}
-		}
-		
-		_lastRegionByLocation.set(location, null);
-		return null;
-	}
+        if (_lastRegionByLocation.keyEquals(location))
+            return _lastRegionByLocation.getValue();
 
-	public List<RentRegion> get(World world) {
+        List<RentRegion> regions = Nucleus.getRegionManager().getRegions(location, RentRegion.class);
 
-		Collection<RentRegion> all = getAll();
-		List<RentRegion> results = new ArrayList<RentRegion>(all.size());
+        if (regions.isEmpty()) {
+            _lastRegionByLocation.set(location, null);
+            return null;
+        }
 
-		for (RentRegion region : all) {
-			if (region.getWorld() == null)
-				continue;
+        RentRegion region = regions.get(0);
+        _lastRegionByLocation.set(location, region);
+        return region;
 
-			if (region.getWorld().equals(world))
-				results.add(region);
-		}
-		return results;
-	}
-	
-	public RentRegion add(String regionName, IRegionSelection selection) {
-		PreCon.notNullOrEmpty(regionName);
-		PreCon.notNull(selection);
+    }
 
-		assert _dataNode != null;
-		RentRegion region = new RentRegion(regionName, _dataNode.getNode(regionName));
-		region.setCoords(selection.getP1(), selection.getP2());
+    public List<RentRegion> get(World world) {
 
-		add(region);
-					
-		return region;
-	}
+        Collection<RentRegion> all = getAll();
+        List<RentRegion> results = new ArrayList<RentRegion>(all.size());
 
-	@Override
-	protected void onRemove(RentRegion removed) {
-		super.onRemove(removed);
-		removed.dispose();
-	}
+        for (RentRegion region : all) {
+            if (region.getWorld() == null)
+                continue;
 
-	@Nullable
-	@Override
-	protected RentRegion load(String name, IDataNode itemNode) {
-		return new RentRegion(name, itemNode);
-	}
+            if (region.getWorld().equals(world))
+                results.add(region);
+        }
+        return results;
+    }
 
-	@Override
-	protected void save(RentRegion item, IDataNode itemNode) {
-		// do nothing
-	}
+    public RentRegion add(String regionName, IRegionSelection selection) {
+        PreCon.notNullOrEmpty(regionName);
+        PreCon.notNull(selection);
+
+        assert _dataNode != null;
+        RentRegion region = new RentRegion(regionName, _dataNode.getNode(regionName));
+        region.setCoords(selection.getP1(), selection.getP2());
+
+        add(region);
+
+        return region;
+    }
+
+    @Override
+    protected void onRemove(RentRegion removed) {
+        super.onRemove(removed);
+        removed.dispose();
+    }
+
+    @Nullable
+    @Override
+    protected RentRegion load(String name, IDataNode itemNode) {
+        return new RentRegion(name, itemNode);
+    }
+
+    @Override
+    protected void save(RentRegion item, IDataNode itemNode) {
+        // do nothing
+    }
 }
