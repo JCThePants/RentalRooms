574707f4f71559d8af216d62789c713a3c2f6a7c
JCThePants <JCThePants@gmail.com>
2015-03-21 13:22:57 -0700

    moved coord related util classes to ...utils.coords package

--- src/com/jcwhatever/rentalrooms/region/RentRegion.java
+++ src/com/jcwhatever/rentalrooms/region/RentRegion.java
@@ -31,7 +31,7 @@ import com.jcwhatever.nucleus.regions.RestorableRegion;
 import com.jcwhatever.nucleus.storage.IDataNode;
 import com.jcwhatever.nucleus.utils.DateUtils;
 import com.jcwhatever.nucleus.utils.Friends;
-import com.jcwhatever.nucleus.utils.LocationUtils;
+import com.jcwhatever.nucleus.utils.coords.LocationUtils;
 import com.jcwhatever.nucleus.utils.MetaKey;
 import com.jcwhatever.nucleus.utils.PreCon;
 import com.jcwhatever.nucleus.utils.Scheduler;
